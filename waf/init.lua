b0ea3f8526ee87b9bd9c423b2ed6fa1503c0dfd0
caobo <boshi_no_1@163.com>
2022-03-01 16:49:52 +0800

    1.修改ifelse为单链表责任链模式； 2.把每个Check改为对象节点，并尽量减少config配置文件对判断体的侵入，做到一定程度上的解耦； 3.优化lua代码，是的代码更加容易阅读和更好的性能； 4.补充了postArgs的判断逻辑，该逻辑需要开启lua_need_request_body，已在README中的WAF部署段添加； 5.修改url.rule中的"在资源文件中的php文件"检测表达式的优化。

--- waf/init.lua
+++ waf/init.lua
@@ -1,118 +1,127 @@
 --WAF Action
-require 'config'
-require 'lib'
+require 'config';
+local lib = require 'lib';
+local ChainList = require 'ChainList';
+local SWITCH_ON = "on";
 
---args
-local rulematch = ngx.re.find
-local unescape = ngx.unescape_uri
+--lib Init
+lib.init(config_rule_dir, config_log_dir, config_waf_output, config_waf_redirect_url, config_output_html);
 
 --allow white ip
-function white_ip_check()
-     if config_white_ip_check == "on" then
-        local IP_WHITE_RULE = get_rule('whiteip.rule')
-        local WHITE_IP = get_client_ip()
+local whiteIp = ChainList.newNode();
+whiteIp.setEnable(SWITCH_ON == config_white_ip_check);
+function whiteIp.run()
+    if whiteIp.enable then
+        local IP_WHITE_RULE = lib.get_rule('whiteip.rule');
+        local WHITE_IP = lib.get_client_ip();
         if IP_WHITE_RULE ~= nil then
             for _,rule in pairs(IP_WHITE_RULE) do
-                if rule ~= "" and rulematch(WHITE_IP,rule,"jo") then
-                    --log_record('White_IP',ngx.var_request_uri,"_","_")
-                    return true
+                if lib.ruleMatch(WHITE_IP,rule) then
+                    --log_record('White_IP',ngx.var_request_uri,"_","_");
+                    return true;
                 end
             end
         end
     end
+    return false;
 end
 
 --deny black ip
-function black_ip_check()
-     if config_black_ip_check == "on" then
-        local IP_BLACK_RULE = get_rule('blackip.rule')
-        local BLACK_IP = get_client_ip()
+local blackIp = ChainList.newNode();
+blackIp.setEnable(SWITCH_ON == config_black_ip_check);
+function blackIp.run()
+    if blackIp.enable then
+        local IP_BLACK_RULE = lib.get_rule('blackip.rule');
+        local BLACK_IP = lib.get_client_ip();
         if IP_BLACK_RULE ~= nil then
             for _,rule in pairs(IP_BLACK_RULE) do
-                if rule ~= "" and rulematch(BLACK_IP,rule,"jo") then
-                    log_record('BlackList_IP',ngx.var_request_uri,"_","_")
-                    if config_waf_enable == "on" then
-                        ngx.exit(403)
-                        return true
-                    end
+                if lib.ruleMatch(BLACK_IP,rule) then
+                    lib.log_record('BlackList_IP',ngx.var_request_uri,"_","_");
+                    ngx.exit(403);
+                    return true;
                 end
             end
         end
     end
+    return false;
 end
 
 --allow white url
-function white_url_check()
-    if config_white_url_check == "on" then
-        local URL_WHITE_RULES = get_rule('whiteurl.rule')
-        local REQ_URI = ngx.var.request_uri
+local whiteUrl = ChainList.newNode();
+whiteUrl.setEnable(SWITCH_ON == config_white_url_check);
+function whiteUrl.run()
+    if whiteUrl.enable then
+        local URL_WHITE_RULES = lib.get_rule('whiteurl.rule');
+        local REQ_URI = ngx.var.request_uri;
         if URL_WHITE_RULES ~= nil then
             for _,rule in pairs(URL_WHITE_RULES) do
-                if rule ~= "" and rulematch(REQ_URI,rule,"jo") then
-                    return true
+                if lib.ruleMatch(REQ_URI,rule) then
+                    return true;
                 end
             end
         end
     end
+    return false;
 end
 
 --deny cc attack
-function cc_attack_check()
-    if config_cc_check == "on" then
-        local ATTACK_URI = ngx.var.uri
-        local CC_TOKEN = get_client_ip()..ATTACK_URI
-        local limit = ngx.shared.limit
-        local CCcount = tonumber(string.match(config_cc_rate,'(.*)/'))
-        local CCseconds = tonumber(string.match(config_cc_rate,'/(.*)'))
-        local req,_ = limit:get(CC_TOKEN)
-        if req then
-            if req > CCcount then
-                log_record('CC_Attack',ngx.var.request_uri,"-","-")
-                if config_waf_enable == "on" then
-                    ngx.exit(403)
-                end
-            else
-                limit:incr(CC_TOKEN,1)
-            end
+local ccAttack = ChainList.newNode();
+ccAttack.setEnable(SWITCH_ON == config_cc_check);
+local cc_rate = string.gmatch('1/2','(%d+)/(%d+)');
+local CCcount, CCseconds = cc_rate();
+ccAttack.CCcount = tonumber(CCcount);
+ccAttack.CCseconds = tonumber(CCseconds);
+function ccAttack.run()
+    if ccAttack.enable then
+        local ATTACK_URI = ngx.var.uri;
+        local CC_TOKEN = lib.get_client_ip()..ATTACK_URI;
+        local limit = ngx.shared.limit;
+        local req, _ = limit:get(CC_TOKEN);
+        if req == nil then
+            limit:set(CC_TOKEN, 1, ccAttack.CCseconds);
+        elseif req > ccAttack.CCcount then
+            lib.log_record('CC_Attack', ngx.var.request_uri, "-", "-");
+            ngx.exit(403);
+            return true;
         else
-            limit:set(CC_TOKEN,1,CCseconds)
+            limit:incr(CC_TOKEN,1);
         end
     end
     return false
 end
 
 --deny cookie
-function cookie_attack_check()
-    if config_cookie_check == "on" then
-        local COOKIE_RULES = get_rule('cookie.rule')
-        local USER_COOKIE = ngx.var.http_cookie
+local cookieAttack = ChainList.newNode();
+cookieAttack.setEnable(SWITCH_ON == config_cookie_check);
+function cookieAttack.run()
+    if cookieAttack.enable then
+        local COOKIE_RULES = lib.get_rule('cookie.rule');
+        local USER_COOKIE = ngx.var.http_cookie;
         if USER_COOKIE ~= nil then
             for _,rule in pairs(COOKIE_RULES) do
-                if rule ~="" and rulematch(USER_COOKIE,rule,"jo") then
-                    log_record('Deny_Cookie',ngx.var.request_uri,"-",rule)
-                    if config_waf_enable == "on" then
-                        waf_output()
-                        return true
-                    end
+                if lib.ruleMatch(USER_COOKIE,rule) then
+                    lib.log_record('Deny_Cookie',ngx.var.request_uri,"-",rule);
+                    lib.waf_output();
+                    return true;
                 end
-             end
-	 end
+            end
+        end
     end
     return false
 end
 
 --deny url
-function url_attack_check()
-    if config_url_check == "on" then
-        local URL_RULES = get_rule('url.rule')
-        local REQ_URI = ngx.var.request_uri
+local urlAttack = ChainList.newNode();
+urlAttack.setEnable(SWITCH_ON == config_url_check);
+function urlAttack.run()
+    if urlAttack.enable then
+        local URL_RULES = lib.get_rule('url.rule');
+        local REQ_URI = ngx.var.request_uri;
         for _,rule in pairs(URL_RULES) do
-            if rule ~="" and rulematch(REQ_URI,rule,"jo") then
-                log_record('Deny_URL',REQ_URI,"-",rule)
-                if config_waf_enable == "on" then
-                    waf_output()
-                    return true
-                end
+            if lib.ruleMatch(REQ_URI, rule) then
+                lib.log_record('Deny_URL',REQ_URI,"-",rule);
+                lib.waf_output();
+                return true;
             end
         end
     end
@@ -120,58 +129,84 @@ function url_attack_check()
 end
 
 --deny url args
-function url_args_attack_check()
-    if config_url_args_check == "on" then
-        local ARGS_RULES = get_rule('args.rule')
+local urlArgsAttack = ChainList.newNode();
+urlArgsAttack.setEnable(SWITCH_ON == config_url_args_check);
+function urlArgsAttack.run()
+    if urlArgsAttack.enable then
+        local ARGS_RULES = lib.get_rule('args.rule');
+        local REQ_ARGS = ngx.req.get_uri_args();
         for _,rule in pairs(ARGS_RULES) do
-            local REQ_ARGS = ngx.req.get_uri_args()
             for key, val in pairs(REQ_ARGS) do
-                if type(val) == 'table' then
-                    local ARGS_DATA = table.concat(val, " ")
-                else
-                    local ARGS_DATA = val
+                local ARGS_DATA = val;
+                if type(ARGS_DATA) == 'table' then
+                    ARGS_DATA = table.concat(ARGS_DATA, " ");
                 end
-                if ARGS_DATA and type(ARGS_DATA) ~= "boolean" and rule ~="" and rulematch(unescape(ARGS_DATA),rule,"jo") then
-                    log_record('Deny_URL_Args',ngx.var.request_uri,"-",rule)
-                    if config_waf_enable == "on" then
-                        waf_output()
-                        return true
-                    end
+                if ARGS_DATA and type(ARGS_DATA) ~= "boolean" and lib.ruleMatch(ngx.unescape_uri(ARGS_DATA),rule) then
+                    lib.log_record('Deny_URL_Args',ngx.var.request_uri,"-",rule);
+                    lib.waf_output();
+                    return true;
                 end
             end
         end
     end
     return false
 end
+
 --deny user agent
-function user_agent_attack_check()
-    if config_user_agent_check == "on" then
-        local USER_AGENT_RULES = get_rule('useragent.rule')
+local userAgentAttack = ChainList.newNode();
+userAgentAttack.setEnable(SWITCH_ON == config_user_agent_check);
+function userAgentAttack.run()
+    if userAgentAttack.enable then
+        local USER_AGENT_RULES = lib.get_rule('useragent.rule')
         local USER_AGENT = ngx.var.http_user_agent
         if USER_AGENT ~= nil then
             for _,rule in pairs(USER_AGENT_RULES) do
-                if rule ~="" and rulematch(USER_AGENT,rule,"jo") then
-                    log_record('Deny_USER_AGENT',ngx.var.request_uri,"-",rule)
-                    if config_waf_enable == "on" then
-                        waf_output()
-                        return true
-                    end
+                if lib.ruleMatch(USER_AGENT,rule) then
+                    lib.log_record('Deny_USER_AGENT',ngx.var.request_uri,"-",rule);
+                    lib.waf_output();
+                    return true;
                 end
             end
         end
     end
-    return false
+    return false;
 end
 
 --deny post
-function post_attack_check()
-    if config_post_check == "on" then
-        local POST_RULES = get_rule('post.rule')
-        for _,rule in pairs(ARGS_RULES) do
-            local POST_ARGS = ngx.req.get_post_args()
+local postAttack = ChainList.newNode();
+postAttack.setEnable(SWITCH_ON == config_post_check);
+function postAttack.run()
+    if postAttack.enable then
+        local POST_RULES = lib.get_rule('post.rule');
+        local POST_ARGS = ngx.req.get_post_args();
+        for _,rule in pairs(POST_RULES) do
+            for key, val in pairs(POST_ARGS) do
+                local ARGS_DATA = val;
+                if type(ARGS_DATA) == 'table' then
+                    ARGS_DATA = table.concat(ARGS_DATA, " ");
+                end
+                if ARGS_DATA and type(ARGS_DATA) ~= "boolean" and lib.ruleMatch(ngx.unescape_uri(ARGS_DATA),rule) then
+                    lib.log_record('Deny_Post_Args',key.."->"..val,"-",rule);
+                    lib.waf_output();
+                    return true;
+                end
+            end
         end
-        return true
+        return true;
     end
-    return false
+    return false;
 end
 
+ChainList.setEnable(SWITCH_ON == config_waf_enable);
+
+ChainList.addNode(whiteIp);
+ChainList.addNode(blackIp);
+ChainList.addNode(userAgentAttack);
+ChainList.addNode(ccAttack);
+ChainList.addNode(cookieAttack);
+ChainList.addNode(whiteUrl);
+ChainList.addNode(urlAttack);
+ChainList.addNode(urlArgsAttack);
+ChainList.addNode(postAttack);
+
+return ChainList;
--- waf/lib.lua
+++ waf/lib.lua
@@ -1,55 +1,70 @@
 --waf core lib
-require 'config'
+local lib = {
+    rule_dir = "",
+    log_dir = "",
+    output = "",
+    redirect_url = "",
+    output_html = "",
+};
+
+--lib construction
+function lib.init(rule_dir, log_dir, waf_output, waf_redirect_url, output_html)
+    lib.rule_dir = rule_dir;
+    lib.log_dir = log_dir;
+    lib.output = waf_output;
+    lib.redirect_url = waf_redirect_url;
+    lib.output_html = output_html;
+end
 
 --Get the client IP
-function get_client_ip()
-    CLIENT_IP = ngx.req.get_headers()["X_real_ip"]
+function lib.get_client_ip()
+    local header = ngx.req.get_headers();
+    local CLIENT_IP = header["X_real_ip"];
     if CLIENT_IP == nil then
-        CLIENT_IP = ngx.req.get_headers()["X_Forwarded_For"]
+        CLIENT_IP = header["X_Forwarded_For"];
     end
     if CLIENT_IP == nil then
-        CLIENT_IP  = ngx.var.remote_addr
+        CLIENT_IP  = ngx.var.remote_addr;
     end
     if CLIENT_IP == nil then
-        CLIENT_IP  = "unknown"
+        CLIENT_IP  = "unknown";
     end
-    return CLIENT_IP
+    return CLIENT_IP;
 end
 
 --Get the client user agent
-function get_user_agent()
-    USER_AGENT = ngx.var.http_user_agent
+function lib.get_user_agent()
+    local USER_AGENT = ngx.var.http_user_agent;
     if USER_AGENT == nil then
-       USER_AGENT = "unknown"
+       USER_AGENT = "unknown";
     end
-    return USER_AGENT
+    return USER_AGENT;
 end
 
 --Get WAF rule
-function get_rule(rulefilename)
-    local io = require 'io'
-    local RULE_PATH = config_rule_dir
-    local RULE_FILE = io.open(RULE_PATH..'/'..rulefilename,"r")
-    if RULE_FILE == nil then
-        return
+function lib.get_rule(rulefilename)
+    local io = require 'io';
+    local RULE_PATH = lib.rule_dir;
+    local RULE_FILE = io.open(RULE_PATH..'/'..rulefilename,"r");
+    local RULE_TABLE = {};
+    if RULE_FILE ~= nil then
+        for line in RULE_FILE:lines() do
+            table.insert(RULE_TABLE, line);
+        end
+        RULE_FILE:close();
     end
-    RULE_TABLE = {}
-    for line in RULE_FILE:lines() do
-        table.insert(RULE_TABLE,line)
-    end
-    RULE_FILE:close()
-    return(RULE_TABLE)
+    return(RULE_TABLE);
 end
 
 --WAF log record for json,(use logstash codec => json)
-function log_record(method,url,data,ruletag)
-    local cjson = require("cjson")
-    local io = require 'io'
-    local LOG_PATH = config_log_dir
-    local CLIENT_IP = get_client_ip()
-    local USER_AGENT = get_user_agent()
-    local SERVER_NAME = ngx.var.server_name
-    local LOCAL_TIME = ngx.localtime()
+function lib.log_record(method,url,data,ruletag)
+    local cjson = require("cjson");
+    local io = require 'io';
+    local LOG_PATH = lib.log_dir;
+    local CLIENT_IP = lib.get_client_ip();
+    local USER_AGENT = lib.get_user_agent();
+    local SERVER_NAME = ngx.var.server_name;
+    local LOCAL_TIME = ngx.localtime();
     local log_json_obj = {
                  client_ip = CLIENT_IP,
                  local_time = LOCAL_TIME,
@@ -59,27 +74,40 @@ function log_record(method,url,data,ruletag)
                  req_url = url,
                  req_data = data,
                  rule_tag = ruletag,
-              }
-    local LOG_LINE = cjson.encode(log_json_obj)
-    local LOG_NAME = LOG_PATH..'/'..ngx.today().."_waf.log"
-    local file = io.open(LOG_NAME,"a")
+              };
+    local LOG_LINE = cjson.encode(log_json_obj);
+    local LOG_NAME = LOG_PATH..'/'..ngx.today().."_waf.log";
+    local file = io.open(LOG_NAME,"a");
     if file == nil then
-        return
+        return;
     end
-    file:write(LOG_LINE.."\n")
-    file:flush()
-    file:close()
+    file:write(LOG_LINE.."\n");
+    file:flush();
+    file:close();
+end
+
+--WAF Match
+function lib.ruleMatch(subject, rule)
+    if rule == nil or rule == "" then
+        return false;
+    end
+    local from, to, error = ngx.re.find(subject,rule,"jo");
+    if from == nil and to == nil then
+        return false;
+    end
+    return true;
 end
 
 --WAF return
-function waf_output()
-    if config_waf_output == "redirect" then
-        ngx.redirect(config_waf_redirect_url, 301)
+function lib.waf_output()
+    if lib.output == "redirect" then
+        ngx.redirect(lib.redirect_url, 301)
     else
         ngx.header.content_type = "text/html"
         ngx.status = ngx.HTTP_FORBIDDEN
-        ngx.say(config_output_html)
+        ngx.say(lib.output_html)
         ngx.exit(ngx.status)
     end
 end
 
+return lib;
